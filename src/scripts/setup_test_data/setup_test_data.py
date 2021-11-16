ae8831ede7a370e10f49884fe8b87d5bdab25909
T. Thyer <tess.thyer@sagebionetworks.org>
2021-11-16 10:36:48 -0800

    Revise setup_test_data script to build up manifest instead of generating it to include metadata; other minor changes for use of later version of synapseformation

--- src/scripts/setup_test_data/setup_test_data.py
+++ src/scripts/setup_test_data/setup_test_data.py
@@ -2,11 +2,13 @@
 This script creates a Synpase project, connects to an existing S3 bucket,
 and syncs test data files to the project.
 '''
-import argparse
 import logging
+import os
 import sys
 
 import boto3
+import numpy as np
+import pandas as pd
 import synapseclient
 from synapseformation import client as synapseformation_client
 import synapseutils
@@ -26,7 +28,9 @@ def get_synapse_client(ssm_parameter):
     Name=ssm_parameter,
     WithDecryption=True)
   syn = synapseclient.Synapse()
-  syn.login(authToken=token['Parameter']['Value'])
+  synapse_auth_token = token['Parameter']['Value']
+  os.environ['SYNAPSE_AUTH_TOKEN'] = synapse_auth_token
+  syn.login(authToken=synapse_auth_token)
   return syn
 
 
@@ -45,9 +49,10 @@ def get_project_id(syn, principal_id):
 
 def create_project(syn, template_path):
   '''Create a synapse project from a template'''
-  log.info(f'Create project {project_name}')
+  log.info(f'Create project {project_name} with template_path {template_path}')
   try:
-    response = synapseformation_client.create_synapse_resources(template_path)
+    response = synapseformation_client.create_synapse_resources(syn, template_path)
+    log.debug(f'Project response: {response}')
     if response is not None:
       return response.get('id')
   except Exception as e:
@@ -55,13 +60,15 @@ def create_project(syn, template_path):
       sys.exit(1)
 
 
-def setup_external_storage(syn, bucket_name, project_id, synapse_folder_name):
+def setup_external_storage(syn, bucket_name, project_id, folder_id):
   '''Connect bucket as external storage for the Synapse project'''
-  log.info(f'Use s3 bucket {bucket_name} as storage for {project_name}')
+  log.info(f'Use s3 bucket {bucket_name} as storage for {project_name}, ' +
+    f'with folder {folder_id}.')
   storage_location = syn.create_s3_storage_location(
           parent = project_id,
-          folder_name = synapse_folder_name,
-          bucket_name = bucket_name)
+          folder = folder_id,
+          bucket_name = bucket_name,
+          sts_enabled=True)
   storage_location_info = {
          k: v for k, v in
          zip(['synapse_folder', 'storage_location', 'synapse_project'],
@@ -70,14 +77,10 @@ def setup_external_storage(syn, bucket_name, project_id, synapse_folder_name):
 
 
 def get_folder_id(syn, project_id, synapse_folder_name):
-  log.info(f'Get synapse id for {synapse_folder_name} folder')
-  response = syn.getChildren(project_id, includeTypes=['folder'])
-  folder = next(
-    filter(
-      lambda x: x['name'] == synapse_folder_name,
-      response
-      ),
-    None)
+  log.info(f'Get synapse id for {synapse_folder_name} folder, child of project {project_id}')
+  response = list(syn.getChildren(project_id, includeTypes=['folder']))
+  folder = next(item for item in response if item['name'] == synapse_folder_name)
+  log.debug(f'folder: {folder}')
   return '' if folder is None else folder.get('id')
 
 def main():
@@ -93,25 +96,34 @@ def main():
   project_id = get_project_id(syn, principal_id)
   if not project_id:
     template_path = './src/scripts/setup_test_data/synapse-formation.yaml'
-    project_id = create_project(syn, template_path)
-
-  # connect bucket and project
-  bucket_name = 'bridge-downstream-dev-source'
-  synapse_folder_name = 'test-data'
-  storage_location_info = setup_external_storage(syn, bucket_name, project_id, synapse_folder_name)
-  log.info(f'storage_location_info: {storage_location_info}')
+    create_project(syn, template_path)
+    project_id = get_project_id(syn, principal_id)
 
   # get folder synapse id
+  synapse_folder_name = 'test-data'
   folder_id = get_folder_id(syn, project_id, synapse_folder_name)
   log.info(f'folder_id: {folder_id}')
 
+  # connect bucket and project
+  bucket_name = 'bridge-downstream-dev-source'
+  storage_location_info = setup_external_storage(syn, bucket_name, project_id, folder_id)
+  log.info(f'storage_location_info: {storage_location_info}')
+
   # generate synapse manifest
-  file_dir_to_sync = './src/scripts/setup_test_data/data'
-  sync_manifest_path = '/tmp/BridgeDownstreamTest-manifest'
-  synapseutils.sync.generate_sync_manifest(syn, file_dir_to_sync, folder_id, sync_manifest_path)
+  data_dir = './src/scripts/setup_test_data/data'
+  manifest_data = np.array([
+    [f'{data_dir}/1Dbywpp4w2zhXjZPgItrKvlh-raw.zip', folder_id, '1Dbywpp4w2zhXjZPgItrKvlh', '|mtb-alpha=NNJ18004|', 'SdVJHmCC8Dcpd_X-sjEK-wH3', '1623421775829', 'FNAME Test Form 1'],
+    [f'{data_dir}/ugJWvqPLhnFV-ifu246CeZjZ-raw.zip', folder_id, 'ugJWvqPLhnFV-ifu246CeZjZ', '|mtb-alpha=MCH20002|', 'vsq7vt_iuT6xjntX09QoQtwk', '1624574992880', 'Vocabulary Form 1'],
+    [f'{data_dir}/R7mBYI63NpCo3sJa9l0qnpza-raw.zip', folder_id, 'R7mBYI63NpCo3sJa9l0qnpza', '|mtb-alpha=NBS22002|', 'nzaLFo5nhlqHc1TyUKhvsBP7', '1625238358060', 'MTB Spelling Form 1'],
+    [f'{data_dir}/Le9-WBxplHhGXo_FLNiY3SaI-raw.zip', folder_id, 'Le9-WBxplHhGXo_FLNiY3SaI', '|mtb-alpha=SBM50004|', 'XrvTSTKobPJAbxGwZ4sceNBf', '1624322928949', 'Vocabulary Form 1']
+  ])
+  df = pd.DataFrame(manifest_data, columns = ['path', 'parent', 'recordid', 'substudymemberships', 'healthcode', 'createdon', 'taskidentifier'])
+  manifest_path = '/tmp/manifest.tsv'
+  df.to_csv(manifest_path, sep = '\t', index = False)
 
   # sync the test data files to Synapse
-  synapseutils.sync.syncToSynapse(syn, sync_manifest_path, dryRun=False, sendMessages=False)
+
+  sync_response = synapseutils.sync.syncToSynapse(syn, manifest_path, dryRun=False, sendMessages=True)
 
 
 if __name__ == "__main__":
