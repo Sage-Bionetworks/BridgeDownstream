34ed903a82523ce4c6c8e071759646695bd32e73
Phil Snyder <subfivemile@gmail.com>
2022-02-24 16:22:49 -0800

    extend functionality of and rename backfill_json_datasets
    In addition to submitting files on Synapse to a workflow, this script
    can now diff those Synapse files with a parquet dataset on S3 before
    submitting to a workflow. The script has been renamed to reflect this
    additional functionality.

--- src/scripts/bootstrap_trigger/bootstrap_trigger.py
+++ src/scripts/bootstrap_trigger/bootstrap_trigger.py
@@ -6,21 +6,30 @@ bucket.
 If --synapse-parent is specified, all files in that folder are submitted to
 the workflow. Otherwise, a --file-view must be specified (and optionally,
 a --query, if you do not want to submit every file in the file view).
+
+If the --diff-* arguments are specified, --file-view must also be specified.
+These arguments allow a parquet dataset in a Synapse-linked S3 location to
+be diffed upon before submitting matching --query results to the --glue-workflow.
+In this way, data which has already been processed by the pipeline is not
+processed again.
 """
 import json
 import argparse
 import boto3
 import synapseclient
+from pyarrow import fs, parquet
 
 
 def read_args():
     parser = argparse.ArgumentParser(
             description=("Submit files on Synapse to an AWS --glue-workflow"))
     parser.add_argument("--synapse-parent",
-                        help="The Synapse ID of the parent folder.")
+                        help="The Synapse ID of the parent folder. "
+                             "Include either this argument or --file-view.")
     parser.add_argument("--file-view",
                         help=("The Synapse ID of a file view containing "
-                              "files to be submitted."))
+                              "files to be submitted. Include either this "
+                              "argument or --synapse-parent"))
     parser.add_argument("--raw-folder-id",
                         required=True,
                         help=("The Synapse ID of a folder containing this "
@@ -29,13 +38,21 @@ def read_args():
                               "Synapse ID of a folder which contains everything "
                               "in the scope). If data is instead coming from the "
                               "folder specified by --synapse-parent, these values "
-                              "are identical.")
+                              "are identical."))
     parser.add_argument("--query",
                         help=("An f-string formatted query which filters the "
                               "file view. Use {source_table} in the FROM clause."))
     parser.add_argument("--glue-workflow",
                         required=True,
                         help="The name of the Glue workflow to submit to.")
+    parser.add_argument("--diff-s3-uri",
+                        help=("The S3 URI of a parquet dataset to diff with "
+                              "before submitting to the --glue-workflow."))
+    parser.add_argument("--diff-parquet-field",
+                        help=("The field name in the parquet dataset to diff "
+                              "upon."))
+    parser.add_argument("--diff-file-view-field",
+                        help=("The field name in the --file-view to diff upon."))
     parser.add_argument("--profile",
                         help="The AWS profile to use.")
     parser.add_argument("--ssm-parameter",
@@ -58,7 +75,7 @@ def get_synapse_client(ssm_parameter=None, aws_session=None):
         syn = synapseclient.login()
     return syn
 
-def get_synapse_ids(syn, synapse_parent=None, entity_view=None, query=None):
+def get_synapse_df(syn, synapse_parent=None, entity_view=None, query=None):
     if synapse_parent is not None: # Submit all files in a folder
         children = syn.getChildren(
                 parent=synapse_parent,
@@ -71,10 +88,30 @@ def get_synapse_ids(syn, synapse_parent=None, entity_view=None, query=None):
             query_string = f"select * from {entity_view}"
         synapse_q = syn.tableQuery(query_string)
         synapse_df = synapse_q.asDataFrame()
-        synapse_ids = synapse_df["id"].values
     else:
         raise ValueError("Either synapse_parent or entity_view must be defined.")
-    return synapse_ids
+    return synapse_df
+
+
+def get_parquet_dataset(dataset_uri, aws_session):
+    session_credentials = aws_session.get_credentials()
+    table_source = dataset_uri.split("s3://")[-1]
+    s3_fs = fs.S3FileSystem(
+            access_key=session_credentials.access_key,
+            secret_key=session_credentials.secret_key,
+            session_token=session_credentials.token)
+    parquet_dataset = parquet.read_table(
+            source=table_source,
+            filesystem=s3_fs)
+    return parquet_dataset
+
+
+def diff_on_parquet(synapse_df, parquet_df, synapse_field, parquet_field):
+    synapse_df = (
+            synapse_df
+            .set_index(synapse_field)
+            .drop(parquet_df[parquet_field].values))
+    return synapse_df.id.values
 
 
 def submit_archives_to_workflow(
@@ -111,17 +148,34 @@ def main():
     syn = get_synapse_client(
             ssm_parameter=args.ssm_parameter,
             aws_session=aws_session)
-    synapse_ids = get_synapse_ids(
+    synapse_df = get_synapse_df(
             syn=syn,
             synapse_parent=args.synapse_parent,
             entity_view=args.file_view,
             query=args.query)
-    submit_archives_to_workflow(
-            syn=syn,
-            synapse_ids=synapse_ids,
-            raw_folder_id=args.raw_folder_id,
-            glue_workflow=args.glue_workflow,
-            aws_session=aws_session)
+    if (
+            args.diff_s3_uri is not None
+            and args.diff_parquet_field is not None
+            and args.diff_file_view_field is not None
+       ):
+        parquet_dataset = get_parquet_dataset(
+                dataset_uri=args.diff_s3_uri,
+                aws_session=aws_session)
+        parquet_index = parquet_dataset.select([args.diff_parquet_field])
+        synapse_ids = diff_on_parquet(
+                synapse_df=synapse_df,
+                parquet_df=parquet_index.to_pandas(),
+                synapse_field=args.diff_file_view_field,
+                parquet_field=args.diff_parquet_field)
+    else:
+        synapse_ids = synapse_df.id.values
+    if len(synapse_ids):
+        submit_archives_to_workflow(
+                syn=syn,
+                synapse_ids=synapse_ids,
+                raw_folder_id=args.raw_folder_id,
+                glue_workflow=args.glue_workflow,
+                aws_session=aws_session)
 
 if __name__ == "__main__":
     main()
