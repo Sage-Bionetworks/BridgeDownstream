c2152139e15b6f3a5c3e167d3d2e7139db372f6a
T. Thyer <tess.thyer@sagebionetworks.org>
2022-02-28 15:56:44 -0800

    Use subprocess instead of os

--- src/scripts/manage_artifacts/artifacts.py
+++ src/scripts/manage_artifacts/artifacts.py
@@ -1,5 +1,5 @@
-import os
 import argparse
+import subprocess
 
 cfn_bucket = 'sceptre-cloudformation-bucket-bucket-65ci2qog5w6l'
 repo_name = 'BridgeDownstream'
@@ -22,35 +22,37 @@ def read_args():
   return args
 
 
+def execute_command(cmd):
+  print(f'Invoking command: {" ".join(cmd)}')
+  subprocess.run(cmd)
+
+
 def upload(namespace):
   '''Copies Glue scripts and CFN templates to the artifacts bucket'''
   scripts_local_path = 'src/glue/'
   scripts_s3_path = f's3://{cfn_bucket}/{repo_name}/{namespace}/glue/'
-  cmd = f'aws s3 sync {scripts_local_path} {scripts_s3_path}'
-  print(f'Invoking command: {cmd}')
-  os.system(cmd)
+  cmd = ['aws', 's3', 'sync', scripts_local_path, scripts_s3_path]
+  execute_command(cmd)
+
 
   templates_local_path = 'templates/'
   templates_s3_path = f's3://{cfn_bucket}/{repo_name}/{namespace}/templates/'
-  cmd = f'aws s3 sync {templates_local_path} {templates_s3_path}'
-  print(f'Invoking command: {cmd}')
-  os.system(cmd)
+  cmd = ['aws', 's3', 'sync', templates_local_path, templates_s3_path]
+  execute_command(cmd)
 
 
 def delete(namespace):
   '''Removes all files recursively for namespace'''
   s3_path = f's3://{cfn_bucket}/{repo_name}/{namespace}/'
-  cmd = f'aws s3 rm --recursive {s3_path}'
-  print(f'Invoking command: {cmd}')
-  os.system(cmd)
+  cmd = ['aws', 's3', 'rm', '--recursive', s3_path]
+  execute_command(cmd)
 
 
 def list_namespaces():
   '''List all namespaces'''
   s3_path = f's3://{cfn_bucket}/{repo_name}/'
-  cmd = f'aws s3 ls {s3_path}'
-  print(f'Invoking command: {cmd}')
-  os.system(cmd)
+  cmd = ['aws','s3','ls', s3_path]
+  execute_command(cmd)
 
 
 def main(namespace):
