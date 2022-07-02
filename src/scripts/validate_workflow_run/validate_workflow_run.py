3fc051643e7818b5f177d6675074db22385ea1d0
mattfazza <accmattfazza@gmail.com>
2022-07-01 22:09:44 -0700

    ETL-184 and ETL-185: addressing PR comments

--- src/scripts/validate_workflow_run/validate_workflow_run.py
+++ src/scripts/validate_workflow_run/validate_workflow_run.py
@@ -4,19 +4,20 @@ import json
 from pyarrow import parquet as pq
 import io
 
-s3 = boto3.resource('s3')
+S3 = boto3.resource('s3')
+
 
 def read_args():
-    #TODO: figure out how key is dynamically formed
+    # TODO: figure out how key is dynamically formed
     """Read command line arguments."""
     parser = argparse.ArgumentParser(
             description=("Builds set of configurations for tests "
-                         "s3://{--bucket}/{--app}/{study}/parquet/archive/"
+                         "S3://{--bucket}/{--app}/{study}/parquet/archive/"
                          "{--dataset}_{--dataset-version}_{update-number}/"))
     parser.add_argument("--bucket",
                         help="The bucket's name.")
     parser.add_argument("--key",
-                        help="The key indentifier for the s3 object.")
+                        help="The key indentifier for the S3 object.")
     parser.add_argument("--profile",
                         help="The AWS profile to use.")
     args = parser.parse_args()
@@ -25,7 +26,7 @@ def read_args():
 
 def get_json_by_key(client, bucket: str, key: str) -> dict:
     """
-    Retrieves a JSON file from s3 using the bucket name and the key
+    Retrieves a JSON file from S3 using the bucket name and the key
 
     Keyword arguments:
     client -- boto3 S3 client
@@ -38,21 +39,22 @@ def get_json_by_key(client, bucket: str, key: str) -> dict:
     try:
         obj = client.get_object(Bucket=bucket, Key=key)
         obj_as_dict = json.loads(obj['Body'].read())
-    except client.exceptions.NoSuchKey as invalid_key:
+    except client.exceptions.NoSuchKey:
         print(f"Key {key} is not valid.")
         return None
-    except client.exceptions.NoSuchBucket as invalid_bucket:
+    except client.exceptions.NoSuchBucket:
         print(f"Bucket {bucket} is not valid.")
         return None
-    except json.decoder.JSONDecodeError as invalid_json:
-        print(f"JSON file downloaded from s3 is not a valid JSON string.")
+    except json.decoder.JSONDecodeError:
+        print("JSON file downloaded from S3 is not a valid JSON string.")
         return None
 
     return obj_as_dict
 
+
 def get_parquet_by_key(client, bucket: str, key: str):
     """
-    Retrieves a parquet file from s3 using the bucket name and the key
+    Retrieves a parquet file from S3 using the bucket name and the key
 
     Keyword arguments:
     client -- boto3 S3 client
@@ -65,17 +67,17 @@ def get_parquet_by_key(client, bucket: str, key: str):
     try:
         obj = client.get_object(Bucket=bucket, Key=key)
         obj_as_parquet = pq.read_table(io.BytesIO(obj['Body'].read()))
-    except client.exceptions.NoSuchKey as invalid_key:
+    except client.exceptions.NoSuchKey:
         print(f"Key {key} is not valid.")
         return None
-    except client.exceptions.NoSuchBucket as invalid_bucket:
+    except client.exceptions.NoSuchBucket:
         print(f"Bucket {bucket} is not valid.")
         return None
-    except TypeError as invalid_parquet:
+    except TypeError:
         print(f"{key} is not a valid parquet file")
         return None
     except Exception as e:
-        print("Unexpected exception in " + e)
+        print("Unexpected exception in " + str(e))
         return None
 
     return obj_as_parquet
@@ -84,8 +86,9 @@ def get_parquet_by_key(client, bucket: str, key: str):
 def main():
     # args = read_args()
     # aws_session = boto3.session.Session(profile_name=args.profile)
-    # s3_client = aws_session.client("s3")
+    # s3_client = aws_session.client("S3")
     pass
+
 
 if __name__ == "__main__":
     main()
