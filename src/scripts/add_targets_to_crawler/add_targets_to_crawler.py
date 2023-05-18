99753c5216ca7ece5082a221679cf2710ec93148
Phil Snyder <subfivemile@gmail.com>
2023-05-18 11:41:49 -0700

    Add logging statements to `add_targets_to_crawler` script

--- src/scripts/add_targets_to_crawler/add_targets_to_crawler.py
+++ src/scripts/add_targets_to_crawler/add_targets_to_crawler.py
@@ -21,6 +21,7 @@ We then update the crawler once more to change the recrawl policy back to crawl 
 folders only.
 """
 import argparse
+import logging
 import json
 import boto3
 
@@ -73,6 +74,11 @@ def read_args():
         default="default",
         help="Which AWS profile to use."
     )
+    parser.add_argument(
+        "--debug",
+        action="store_true",
+        help="Set the log level to debug."
+    )
     args = parser.parse_args()
     return args
 
@@ -115,7 +121,7 @@ def add_targets_to_crawler(
         crawler_name: str,
         s3_paths: list,
         record_state=False
-    ) -> dict:
+    ):
     """
     Adds a target to a Glue crawler.
 
@@ -129,19 +135,21 @@ def add_targets_to_crawler(
             Defaults to False.
 
     Returns:
-        dict: The response from the Glue service after updating the crawler.
-
-    Raises:
-        Boto3Error: If there is an error calling the Glue service.
+        (None)
     """
+    logger = logging.getLogger(__name__)
     glue_client = aws_session.client("glue")
     # Prepare initial crawler update with recrawl behavior set to `CRAWL_EVERYTHING_POLICY`
     # so that Glue permits us to update the targets.
-    crawler_update = prepare_crawler_update(
-            aws_session=aws_session,
-            crawler_name=crawler_name,
-            record_state=record_state
-    )
+    try:
+        crawler_update = prepare_crawler_update(
+                aws_session=aws_session,
+                crawler_name=crawler_name,
+                record_state=record_state
+        )
+    except glue_client.exceptions.EntityNotFoundException:
+        logger.error("Crawler with name %s does not exist", crawler_name)
+        return
     original_recrawl_behavior = crawler_update["RecrawlPolicy"]["RecrawlBehavior"]
     crawler_update["RecrawlPolicy"] = {"RecrawlBehavior": CRAWL_EVERYTHING_POLICY}
     # Add S3 targets to the crawler update
@@ -151,35 +159,52 @@ def add_targets_to_crawler(
             "Exclusions": []
         })
     # Update the crawler with new targets
-    response = glue_client.update_crawler(**crawler_update)
+    try:
+        logger.info("Adding S3 targets to crawler %s", crawler_name)
+        response = glue_client.update_crawler(**crawler_update)
+        logger.debug(response)
+    except glue_client.exceptions.InvalidInputException as e:
+        logger.error("Something went wrong with the crawler update: %s", crawler_update)
+        logger.error(str(e))
+        return
     # If we changed the recrawl policy, change it back
     if original_recrawl_behavior != CRAWL_EVERYTHING_POLICY:
+        logger.debug("Setting crawler to use recrawl policy %s", original_recrawl_behavior)
         crawler_update = prepare_crawler_update(
                 aws_session=aws_session,
                 crawler_name=crawler_name
         )
         crawler_update["RecrawlPolicy"] = {"RecrawlBehavior": original_recrawl_behavior}
         response = glue_client.update_crawler(**crawler_update)
+        logger.debug(response)
     # Save the updated crawler details to a JSON file
-    with open(f"{crawler_name}_updated.json", "w") as f:
-        crawler_update = glue_client.get_crawler(Name=crawler_name)["Crawler"]
-        json.dump(crawler_update, f, default=str)
-    return response
+    if record_state:
+        with open(f"{crawler_name}_updated.json", "w") as f:
+            crawler_update = glue_client.get_crawler(Name=crawler_name)["Crawler"]
+            json.dump(crawler_update, f, default=str)
 
 def main():
     args = read_args()
+    if args.debug:
+        logging.basicConfig(level=logging.DEBUG)
+    else:
+        logging.basicConfig()
+    logger = logging.getLogger(__name__)
     aws_session = boto3.Session(profile_name=args.aws_profile)
     if args.environment == "develop":
         bucket_name = "bridge-downstream-dev-intermediate-data"
     elif args.environment == "prod":
         bucket_name = "bridge-downstream-intermediate-data"
+    logger.debug("Using bucket %s", bucket_name)
     for study in args.studies:
+        logger.debug("Beginning update for study %s", study)
         crawler_name = CRAWLER_FORMAT.format(
                 namespace=args.namespace,
                 app=args.app,
                 study=study,
                 crawler_type=args.crawler_type
         )
+        logger.debug("Crawler name: %s", crawler_name)
         s3_paths = [
                 TARGET_FORMAT.format(
                     bucket=bucket_name,
@@ -189,6 +214,7 @@ def main():
                     dataset=dataset
                 ) for dataset in args.datasets
         ]
+        logger.debug("Targets: %s", s3_paths)
         add_targets_to_crawler(
                 aws_session=aws_session,
                 crawler_name=crawler_name,
